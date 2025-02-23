2e41255d3e43b417843bd84aa860adf540b86f3a
Sumit <sumitadhikari2341@gmail.com>
2025-02-23 15:26:41 +0900

    Javascript updated

--- sumit jan 5/JS/script.js
+++ sumit jan 5/JS/script.js
@@ -1,9 +1,21 @@
-let firstName="Sumit"
-let lastName="Adhikari"
-let fullName= firstName+lastName;
+let jMass= 78;
+let jheight= 1.69;
+let jresult = jMass/jheight**2;
+console.log(jresult);
 
-console.log(fullName);
+let mkmass=95;
+let mkheight=1.76;
+let mkresult= mkmass/mkheight**2;
+console.log(mkresult);
+
+let fresult= mkresult > jresult;
+console.log(fresult);
 
 
 
-
+if(mkresult>jresult){
+    console.log('markBMI is greater');
+}
+else{
+    console.log('JohnBMI is greater')
+}
