fbd82dfc9086cab91796310af397f878af9d4d19
Sumit <sumitadhikari2341@gmail.com>
2025-03-09 11:08:39 +0900

    if else and number

--- sumit jan 5/JS/script.js
+++ sumit jan 5/JS/script.js
@@ -11,11 +11,15 @@ console.log(mkresult);
 let fresult= mkresult > jresult;
 console.log(fresult);
 
-
-
 if(mkresult>jresult){
     console.log('markBMI is greater');
 }
 else{
     console.log('JohnBMI is greater')
 }
+
+let number1=prompt("Enter the first Numbar");
+let number2=prompt("Enter the second Number");
+
+let result=Number(number1)+Number(number2);
+alert(result);
