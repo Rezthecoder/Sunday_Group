8e4b9581d86051dc14673011ef1439aa495cfc21
Sumit <sumitadhikari2341@gmail.com>
2025-03-16 16:09:13 +0900

    switch studied

--- sumit jan 5/JS/script.js
+++ sumit jan 5/JS/script.js
@@ -24,13 +24,139 @@
 // let result=Number(number1)+Number(number2);
 // alert(result);
 
-let name= "Sumit";
-let surname="Adhikari";
-let age= 24;
-let title="front-end developer";
-let salary= "4lakh";
+// let name= "Sumit";
+// let surname="Adhikari";
+// let age= 24;
+// let title="front-end developer";
+// let salary= "4lakh";
 
-let concat= 'My name is '+ name + surname + " i'm"+ age +' years old.'+ " i'm"+title + ' my salary is'+ salary+ 'per month.'
-console.log(concat);
-let temp= `My name is ${name} ${surname}. i'm ${age} years old. i'm ${title}. My Salary is ${salary} per month.`
-console.log(temp);
+// let concat= 'My name is '+ name + surname + " i'm"+ age +' years old.'+ " i'm"+title + ' my salary is'+ salary+ 'per month.'
+// console.log(concat);
+// let temp= `My name is ${name} ${surname}. i'm ${age} years old. i'm ${title}. My Salary is ${salary} per month.`
+// console.log(temp);
+
+'use strict'
+// let hasDrivinglisence = false;
+// let hasPass = true;
+
+// if(hasPass){
+//     hasDrivinglisence = true;
+// }
+// if(hasDrivinglisence){
+//     console.log("Now you can drive vehicles");
+// }
+
+//  let day = prompt("Enter a day")
+
+//  switch (day) {
+//     case 'sunday':
+//         alert("beer khane")
+//         break;
+//         case 'monday':
+//         alert("pani khane")
+//         break;
+//         case 'tuesday':
+//         alert("wiskey khane")
+//         break;
+//         case 'wednesday':
+//         alert("rum khane")
+//         break;
+//         case 'thursday':
+//         alert("wine khane")
+//         break;
+//         case 'friday':
+//         alert("juice khane")
+//         break;
+//         case 'saturday':
+//         alert("monostor khane")
+//         break;
+ 
+//     default: alert("kei ta khanu parxa yr")
+//         break;
+//  }
+// ---------------------------------------------------------------------------------------
+
+//  let day = prompt("Enter a day")
+
+//  switch (day) {
+//     case 'sunday':
+//         console.log("beer khane")
+//         break;
+//         case 'monday':
+//         console.log("pani khane")
+//         break;
+//         case 'tuesday':
+//         console.log("wiskey khane")
+//         break;
+//         case 'wednesday':
+//         console.log("rum khane")
+//         break;
+//         case 'thursday':
+//         console.log("wine khane")
+//         break;
+//         case 'friday':
+//         console.log("juice khane")
+//         break;
+//         case 'saturday':
+//         console.log("monostor khane")
+//         break;
+ 
+//     default: console.log("kei ta khanu parxa yr")
+//  }
+// -----------------------------------------------------------------------------------
+// let day = prompt("Enter a day")
+//  switch (day) {
+//     case 'sunday':
+//         document.getElementById('day').textContent ="beer khane";
+      
+//         break;
+//         case 'monday':
+//             document.getElementById('day').textContent = "pani khane!";
+//         break;
+//         case 'tuesday':
+//             document.getElementById('day').textContent = "wiskey khane!";
+//         break;
+//         case 'wednesday':
+//             document.getElementById('day').textContent = "rum khane!";
+//         break;
+//         case 'thursday':
+//             document.getElementById('day').textContent = "wine khane!";
+//         break;
+//         case 'friday':
+//             document.getElementById('day').textContent = "juice khane!";
+//         break;
+//         case 'saturday':
+//             document.getElementById('day').textContent = "monostor khane!";
+//         break;
+ 
+//     default:  document.getElementById('day').textContent ="kei ta khanu parxa yr";
+//         break;
+//  }
+let day= prompt("Enter a day")
+let message = document.getElementById('day')
+switch (day) {
+        case 'sunday':
+            message.textContent ="beer khane";
+            break;
+            case 'monday':
+                message.textContent = "pani khane!";
+            break;
+            case 'tuesday':
+                message.textContent = "wiskey khane!";
+            break;
+            case 'wednesday':
+                message.textContent = "rum khane!";
+            break;
+            case 'thursday':
+                message.textContent = "wine khane!";
+            break;
+            case 'friday':
+                message.textContent = "juice khane!";
+            break;
+            case 'saturday':
+                message.textContent = "monostor khane!";
+            break;
+     
+        default:  message.textContent ="kei ta khanu parxa yr";
+            break;
+     }
